ff005a4d0b0d014b771e264936993e0199186638
Damien Gonot <dgonot@wealthsimple.com>
2021-12-01 12:06:11 -0500

    better julia day 1 using sum and zip

--- day-01/solution.jl
+++ day-01/solution.jl
@@ -2,8 +2,8 @@ using DelimitedFiles
 using Transducers
 
 input = readdlm("input", Int) |> vec
-compare_reduce(vector) = reduce(+, map(x -> x[1] - 1 == 0 ? false : x[2] > vector[x[1] - 1], enumerate(vector)))
+compare(vec) = map(((x, y),) -> x < y, zip(vec, vec[2:end]))
 windows = input |> Transducers.Partition(3; step = 1) |> Map(copy) |> collect
 
-println("Part 1: ", compare_reduce(input))
-println("Part 2: ", compare_reduce(sum.(windows)))
+println("Part 1: ", input |> compare |> sum)
+println("Part 2: ", sum.(windows) |> compare |> sum)
