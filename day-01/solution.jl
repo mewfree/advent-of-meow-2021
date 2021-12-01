7e69fbe89bb5b4d1ec8e60b7984e107026fe6a61
Damien Gonot <damien.gonot@gmail.com>
2021-12-01 08:12:48 -0500

    julia day 1 part 2

--- day-01/solution.jl
+++ day-01/solution.jl
@@ -1,4 +1,9 @@
 using DelimitedFiles
+using Transducers
+
 input = readdlm("input", Int) |> vec
-solution = reduce(+, map(x -> x[1] - 1 == 0 ? false : x[2] > input[x[1] - 1], enumerate(input)))
-println("Part 1: ", solution)
+compare_reduce(vector) = reduce(+, map(x -> x[1] - 1 == 0 ? false : x[2] > vector[x[1] - 1], enumerate(vector)))
+windows = input |> Transducers.Partition(3; step = 1) |> Map(copy) |> collect
+
+println("Part 1: ", compare_reduce(input))
+println("Part 2: ", compare_reduce(sum.(windows)))
