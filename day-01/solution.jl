c9764fdc2187aba9d20eb319feac5211c792428c
Damien Gonot <dgonot@wealthsimple.com>
2021-12-01 13:40:15 -0500

    better julia day 1 using diff

--- day-01/solution.jl
+++ day-01/solution.jl
@@ -2,7 +2,7 @@ using DelimitedFiles
 using Transducers
 
 input = readdlm("input", Int) |> vec
-compare(vec) = map(((x, y),) -> x < y, zip(vec, vec[2:end]))
+compare(vec) = map(x -> x > 0, diff(vec))
 windows = input |> Transducers.Partition(3; step = 1) |> Map(copy) |> collect
 
 println("Part 1: ", input |> compare |> sum)
