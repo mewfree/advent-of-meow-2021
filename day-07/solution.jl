3389189c611413d00dfdd135974f6e47ce0f02f9
Damien Gonot <damien.gonot@gmail.com>
2021-12-11 12:03:06 -0500

    julia day 7 part 2

--- day-07/solution.jl
+++ day-07/solution.jl
@@ -3,4 +3,9 @@ using DelimitedFiles
 input = readdlm("input", ',', Int) |> vec
 
 fuel(x, input) = map(i -> abs(i - x), input) |> sum
-println("Part 1: ", map(i -> fuel(i, input), minimum(input):maximum(input)) |> minimum)
+fuel2(x, input) = map(i -> sum(1:abs(i - x)), input) |> sum
+
+solve(f, input) = map(i -> f(i, input), minimum(input):maximum(input)) |> minimum
+
+println("Part 1: ", solve(fuel, input))
+println("Part 2: ", solve(fuel2, input))
